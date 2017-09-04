532f62ba4123e9a3d06da3c5a649620c809455a5
Trung Ly <trung.ly@cit3-501m-01.auckland.natcoll.ac.nz>
2017-09-04 15:31:41 +1200

    adding extend.scss

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,15 @@
  
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
  
 gulp.task('sass', function () {
   return gulp.src('./scss/**/*.scss')
+
+  	.pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write('../maps'))
+
     .pipe(gulp.dest('./css'));
 });
  
